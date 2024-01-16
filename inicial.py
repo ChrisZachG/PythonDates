c0e7193dddcab375287389870d3eaf79ae82077c
Chris <chriszavalag@ciencias.unam.mx>
2024-01-16 01:55:38 -0600

    Es el primer commit del archivo :)

---
+++ inicial.py
@@ -0,0 +1,24 @@
+print("No es la primera vez que hago esto pero imaginemos que si")
+
+#Datos simples
+"string" #cadena de texto
+"""tus datos son:
+nombre: Garp
+    id:pascdart """
+int("40") #enteros
+float("48.2") #flotantes
+bool("false") #boolean
+
+#Variables
+x = "8"
+nombre  = "Garp"
+numero = 10 + 5
+numero += 1 #el valor que ya tiene más lo que este despues del igual
+numero -= 1 #el valor que tiene menos lo que esta despues del igual
+bienvenida = "Hola " + nombre + " ¿como estas?"
+bienvenida2 = f"Hola {nombre} ¿que tal tu dia?" #f strings
+print(x)
+print(nombre)
+print(numero)
+print(bienvenida)
+print(bienvenida2)
